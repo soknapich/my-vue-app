e3632001d6ff1b79937940c986f4a5e2261a6ff3
Sokna <soknait@gmail.com>
2025-12-06 17:04:59 +0700

    fixed

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,26 +1,33 @@
 <template>
-  <div class="loading-overlay">
-      <div class="loader inline-block w-6 h-6 mr-2 align-middle"></div>
+  <!-- Dialog -->
+  <div class="fixed inset-0 bg-black/30 backdrop-blur-sm flex items-center justify-center z-50">
+    <div class="bg-white p-6 rounded shadow-lg w-lg">
+      <div class="text-lg font-semibold mt-4 mb-4 text-gray-800">
+        <div class="loader inline-block w-6 h-6 mr-2 align-middle"></div>
+        <span>{{ title }}</span>
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
-.loading-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: rgba(0,0,0,0.3);
-  z-index: 9999;
-}
+<script setup lang="ts">
 
+defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
+  title: {
+    type: String,
+    default: "Working...",
+  },
+});
+</script>
+
+<style>
 .loader {
-  width: 40px;
-  height: 40px;
+  width: 50px;
+  height: 50px;
   border: 5px solid #eee;
   border-top-color: #42b983;
   border-radius: 50%;
