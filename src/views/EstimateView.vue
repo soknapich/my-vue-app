eb815b4ea1ec4d755034a7f0cd2b94ba6aa462b3
Sokna Pich <p.sokna@JACCS.COM.KH>
2026-02-04 13:11:40 +0700

    Add action expport

--- src/views/EstimateView.vue
+++ src/views/EstimateView.vue
@@ -3,62 +3,81 @@
     <div class="overflow-x-auto">
         <div class="card bg-white text-gray-800 rounded-lg shadow p-2">
 
-            <div class="card flex flex-wrap justify-center gap-4 mb-4">
-                <div class="flex items-center gap-2">
-                    <Checkbox v-model="estimate_or_actual" inputId="ingredient1" name="estimate_or_actual"
-                        value="Estimate" size="small" />
-                    <label for="ingredient1"> Estimate </label>
-                </div>
-                <div class="flex items-center gap-2">
-                    <Checkbox v-model="estimate_or_actual" inputId="ingredient2" name="estimate_or_actual"
-                        value="Actual" size="small" />
-                    <label for="ingredient2"> Actual </label>
-                </div>
-                <!-- <div class="flex items-center gap-2">
-                    <Checkbox v-model="estimate_or_actual" inputId="ingredient3" name="estimate_or_actual"
-                        value="Margin" size="small" />
-                    <label for="ingredient3"> Margin </label>
-                </div> -->
-                <div class="flex items-center gap-2">
-                    <!-- <Button icon="pi pi-plus" title="New" rounded size="small" @click="downloadExcel();" /> -->
-                    <Button icon="pi pi-file-excel" title="Export Excel" rounded size="small" @click="downloadExcel();" />
-                </div>
-
+        <div class="grid grid-cols-12 gap-1">
+            <div class="col-span-12 md:col-span-6">
+                  <div class="flex flex-wrap justify-start gap-4 mb-4">
+                      <div class="flex items-center gap-2">
+                          <Checkbox v-model="estimate_or_actual" inputId="ingredient1" name="estimate_or_actual"
+                              value="Estimate" size="small" />
+                          <label for="ingredient1"> Estimate </label>
+                      </div>
+                      <div class="flex items-center gap-2">
+                          <Checkbox v-model="estimate_or_actual" inputId="ingredient2" name="estimate_or_actual"
+                              value="Actual" size="small" />
+                          <label for="ingredient2"> Actual </label>
+                      </div>
+                  </div>
             </div>
+            <div class="col-span-12 md:col-span-6">
+                  <div class="flex flex-wrap justify-end gap-4 mb-4">
+                      <div class="flex items-center gap-2">
+                          <!-- <Button icon="pi pi-plus" title="New" rounded size="small" @click="downloadExcel();" /> -->
+                          <Button title="Export Excel"  size="small" @click="downloadExcel();">
+                            <i class="pi pi-cloud-download"></i>Export
+                          </Button>
+                      </div>
+                  </div>
+            </div>
+        </div>
+        <!-- Context Menu -->
+        <ContextMenu ref="menu" :model="items" />
+        <TreeTable
+            :value="nodes"
+            lazy
+            size="small"
+            selectionMode="single"
+            @node-expand="onNodeExpand"
 
-            <TreeTable :value="nodes" lazy size="small" @node-expand="onNodeExpand" selectionMode="single"
-                tableStyle="min-width: 50rem">
-                <Column field="title" header="បរិយាយ" expander style="width: 40%">
-                    <template #body="{ node }">
-                        <div class="flex align-items-center gap-1">
-                            <!-- Icon -->
-                            <i :class='node.icon' class="text-success"></i>
-                            <!-- Text -->
-                            <small>{{ node.data.title }}</small>
-                        </div>
-                    </template>
-                </Column>
-                <!-- <Column field="spec" header="Spec"></Column>
-                <Column field="brand" header="ម៉ាក"></Column>
-                <Column field="size" header="ទំហំ"></Column>
-                <Column field="unit" header="ឯកតា"></Column> -->
-                <Column field="qty" header="បរិមាណ" v-if="estimate_or_actual.includes('Estimate')"></Column>
-                <Column field="material" header="តម្លៃសម្ភារ" v-if="estimate_or_actual.includes('Estimate')"></Column>
-                <Column field="labor" header="តម្លៃពលកម្ម" v-if="estimate_or_actual.includes('Estimate')"></Column>
-                <Column field="total" header="សរុប" v-if="estimate_or_actual.includes('Estimate')"></Column>
+            tableStyle="min-width: 50rem">
+            <Column field="title" header="បរិយាយ" expander style="width: 40%">
+                <template #body="{ node }">
+                    <div class="flex align-items-center gap-1">
+                        <!-- Icon -->
+                        <!-- <i :class='node.icon' class="text-success"></i> -->
+                        <span v-if="node.icon !== 'pi pi-star'">📁</span>
+                        <!-- Text -->
+                        <small>{{ node.data.title }}</small>
+                    </div>
+                </template>
+            </Column>
+            <!-- <Column field="spec" header="Spec"></Column>
+            <Column field="brand" header="ម៉ាក"></Column>
+            <Column field="size" header="ទំហំ"></Column>
+            <Column field="unit" header="ឯកតា"></Column> -->
+            <Column field="qty" header="បរិមាណ" v-if="estimate_or_actual.includes('Estimate')"></Column>
+            <Column field="material" header="តម្លៃសម្ភារ" v-if="estimate_or_actual.includes('Estimate')"></Column>
+            <Column field="labor" header="តម្លៃពលកម្ម" v-if="estimate_or_actual.includes('Estimate')"></Column>
+            <Column field="total" header="សរុប" v-if="estimate_or_actual.includes('Estimate')"></Column>
 
 
-                <Column field="actual_material" header="តម្លៃសម្ភារជាក់ស្តែង"
-                    v-if="estimate_or_actual.includes('Actual')"></Column>
-                <Column field="actual_labor" header="តម្លៃពលកម្មជាក់ស្តែង" v-if="estimate_or_actual.includes('Actual')">
-                </Column>
-                <Column field="actual_total" header="សរុបជាក់ស្តែង" v-if="estimate_or_actual.includes('Actual')">
-                </Column>
+            <Column field="actual_material" header="តម្លៃសម្ភារជាក់ស្តែង"
+                v-if="estimate_or_actual.includes('Actual')"></Column>
+            <Column field="actual_labor" header="តម្លៃពលកម្មជាក់ស្តែង" v-if="estimate_or_actual.includes('Actual')">
+            </Column>
+            <Column field="actual_total" header="សរុបជាក់ស្តែង" v-if="estimate_or_actual.includes('Actual')">
+            </Column>
 
-                <!-- <Column field="material" header="តម្លៃសម្ភារ" v-if="estimate_or_actual.includes('Margin')"></Column>
-                <Column field="labor" header="តម្លៃពលកម្ម" v-if="estimate_or_actual.includes('Margin')"></Column>
-                <Column field="total" header="សរុប" v-if="estimate_or_actual.includes('Margin')"></Column> -->
-            </TreeTable>
+            <!-- <Column field="material" header="តម្លៃសម្ភារ" v-if="estimate_or_actual.includes('Margin')"></Column>
+            <Column field="labor" header="តម្លៃពលកម្ម" v-if="estimate_or_actual.includes('Margin')"></Column>
+            <Column field="total" header="សរុប" v-if="estimate_or_actual.includes('Margin')"></Column> -->
+
+            <Column field="export" header="Action">
+              <template #body="slotProps">
+                <Button icon="pi pi-cloud-download" v-if="slotProps.node.icon!=='pi pi-star'" rounded text @click="downloadExcelByPlanId(slotProps.node.data.id, slotProps.node.icon, slotProps.node.data.title)"/>
+              </template>
+            </Column>
+
+        </TreeTable>
         </div>
     </div>
 </template>
@@ -90,6 +109,7 @@ const onNodeExpand = async (node) => {
 onMounted(async () => {
     loadRoot();
 });
+
 const toNumber = (val) => Number(val.replace(/,/g, ""));
 
 const loadRoot = async () => {
@@ -179,5 +199,19 @@ const loadRoot = async () => {
 
 async function downloadExcel() {
     await exportExcell('plan/export-excell', 'project-export-all');
-}
+};
+
+async function downloadExcelByPlanId(pid, icon, title) {
+   //console.log(icon);
+   if(icon === 'pi pi-th-large'){
+      await exportExcell(`house/export-excell?plan_id=${pid}`, `${title}`);
+   }else if(icon === 'pi pi-home'){
+      await exportExcell(`boq-level-one/export-excell?house_id=${pid}`, `${title}`);
+   }else if(icon === 'pi pi-folder'){
+      //await exportExcell(`boq-level-two/export-excell?parent_id=${pid}`, `${title}`);
+   }else if(icon === 'pi pi-book'){
+      await exportExcell(`boq-item/export-excell?level_id=${pid}`, `${title}`);
+   }
+
+};
 </script>
